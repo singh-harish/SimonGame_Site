f6de13e863d209f9a4d82bb3de3dd34ac141c975
Harish Singh <singhharish734@gmail.com>
2021-12-27 12:14:17 +0530

    flash added

---
+++ game.js
@@ -0,0 +1,13 @@
+var gamePattern= [];
+var buttonColor= [ "red", "blue", "green", "yellow" ];
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour= buttonColor[randomNumber];
+  gamePattern.push(randomChosenColour);
+  
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio= new Audio("sounds/"+ randomChosenColour + ".mp3");
+  audio.play();
+}
