9d3cefa391e1e2f6c9ae37b2d587825b5e36ec56
Harish Singh <singhharish734@gmail.com>
2021-12-29 19:04:57 +0530

    animation added

--- game.js
+++ game.js
@@ -2,26 +2,44 @@ var userClickedPattern= [];
 var gamePattern= [];
 var buttonColor= [ "red", "blue", "green", "yellow" ];
 
+var started= false;
+var level= 0;
+
 $(".btn").click(function(event){
   var userChosenColour = $(this).attr('id');
   userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
-
-
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random()*4);
-  var randomChosenColour= buttonColor[randomNumber];
-  gamePattern.push(randomChosenColour);
-
-  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
-}
-
 
 function playSound(name) {
   var audio= new Audio("sounds/"+ name + ".mp3");
   audio.play();
 }
 
-// playSound(userChosenColour);
-nextSequence();
+function nextSequence(){
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour= buttonColor[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  return randomChosenColour;
+}
+
+function animatePress(currentColor){
+  $("#"+currentColor).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColor).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function(){
+  $("#level-title").text("Level "+level);
+  nextSequence();
+  started= true;
+});
