ce8dfa313241b8bc63c84a71349340a447d28c60
Harish Singh <singhharish734@gmail.com>
2021-12-29 18:20:42 +0530

    audio added

--- game.js
+++ game.js
@@ -1,13 +1,27 @@
+var userClickedPattern= [];
 var gamePattern= [];
 var buttonColor= [ "red", "blue", "green", "yellow" ];
+
+$(".btn").click(function(event){
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+});
+
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour= buttonColor[randomNumber];
   gamePattern.push(randomChosenColour);
-  
-  $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio= new Audio("sounds/"+ randomChosenColour + ".mp3");
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+
+function playSound(name) {
+  var audio= new Audio("sounds/"+ name + ".mp3");
   audio.play();
 }
+
+// playSound(userChosenColour);
+nextSequence();
