ef9175f9fbb6683712e8cac74bd32b0bf5fadbad
Harish Singh <singhharish734@gmail.com>
2021-12-31 22:55:09 +0530

    pre final

--- game.js
+++ game.js
@@ -1,49 +1,78 @@
-var userClickedPattern= [];
-var gamePattern= [];
-var buttonColor= [ "red", "blue", "green", "yellow" ];
 
-var started= false;
-var level= 0;
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-$(".btn").click(function(event){
-  var userChosenColour = $(this).attr('id');
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
 
-function playSound(name) {
-  var audio= new Audio("sounds/"+ name + ".mp3");
-  audio.play();
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
 }
 
-function nextSequence(){
+function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
-
-  var randomNumber = Math.floor(Math.random()*4);
-  var randomChosenColour= buttonColor[randomNumber];
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  return randomChosenColour;
 }
 
-function animatePress(currentColor){
-  $("#"+currentColor).addClass("pressed");
-  setTimeout(function(){
-    $("#"+currentColor).removeClass("pressed");
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
-function checkAnswer(currentLevel){
-  
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
-$(document).keypress(function(){
-  $("#level-title").text("Level "+level);
-  nextSequence();
-  started= true;
-});
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
